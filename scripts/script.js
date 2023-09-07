ae90f9dd91ed41a149cfabfb7f7b782ccb18b9a9
OvenMagnetron <jessekramer15@gmail.com>
2023-09-07 12:44:35 +0200

    Achterkant klaar, css, java etc aangepast

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
 function myFlipFunction() {
   var myElement = document.getElementById("thecard");
-  if (myElement.classList.contains("flipper")) {
-    myElement.classList.remove("flipper");
-  } else {
-    myElement.classList.remove("floating");
-    myElement.classList.add("flipper");
+  myElement.classList.toggle("flipper");
+
+  var buttonElement = document.querySelector('.button');
+
+  if (buttonElement) {
+    var currentColor = buttonElement.style.color;
+    console.log(currentColor)
+
+    if (currentColor === 'rgb(229, 225, 225)' || currentColor === '') {
+      buttonElement.style.color = '#252525';
+    } else {
+      buttonElement.style.color = '#E5E1E1';
+    }
   }
+
 }
